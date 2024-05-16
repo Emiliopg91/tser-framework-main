acc6c4f7ecc10802f82ff48838480d19b8901173
Emilio <ojosdeserbio@gmail.com>
2024-05-16 15:12:46 +0200

    ProtocolBindings

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ export { LoggerMain } from "./implementations/LoggerMain";
 
 export { AppConfig } from "./types/AppConfig";
 export { IpcListener, defaultIpcListeners } from "./types/IpcListener";
+export {
+  ProtocolBinding,
+  defaultProtocolBindings,
+} from "./types/ProtocolBinding";
 export { WindowConfig } from "./types/WindowConfig";
---
+++ src/types/ProtocolBinding.ts
@@ -0,0 +1,21 @@
+import { Privileges, net } from "electron";
+import { OSHelper } from "../implementations/OSHelper";
+import url from "url";
+
+export interface ProtocolBinding {
+  privileges: Privileges;
+  handler: (request: GlobalRequest) => GlobalResponse | Promise<GlobalResponse>;
+}
+
+export const defaultProtocolBindings: Record<string, ProtocolBinding> = {
+  local: {
+    privileges: { bypassCSP: true },
+    handler: (request) => {
+      let filePath = request.url.slice("local://".length);
+      if (OSHelper.isWindows()) {
+        filePath = filePath.substring(0, 1) + ":" + filePath.substring(1);
+      }
+      return net.fetch(url.pathToFileURL(filePath).toString());
+    },
+  },
+};
