22b2d3454e32f04d40cf413db2ab170c3260a4c2
Emilio <ojosdeserbio@gmail.com>
2024-05-19 23:46:10 +0200

    no message

--- src/implementations/TrayHelper.ts
+++ src/implementations/TrayHelper.ts
@@ -1,11 +1,12 @@
 import { Menu, MenuItemConstructorOptions, Tray, nativeImage } from "electron";
+import { JsonUtils } from "@tser-framework/commons";
 import { LoggerMain } from "./LoggerMain";
 import { TranslatorMain } from "./TranslatorMain";
 
 export class TrayBuilder {
   private iconPath: string;
   private toolTip: string | null = null;
-  private contextMenu: TrayMenuBuilder | null = null;
+  private contextMenu: Array<MenuItemConstructorOptions> | null = null;
 
   public static builder(iconPath: string): TrayBuilder {
     return new TrayBuilder(iconPath);
@@ -20,7 +21,7 @@ export class TrayBuilder {
     return this;
   }
 
-  public withContextMenu(menu: TrayMenuBuilder): TrayBuilder {
+  public withMenu(menu: Array<MenuItemConstructorOptions>): TrayBuilder {
     this.contextMenu = menu;
     return this;
   }
@@ -28,52 +29,25 @@ export class TrayBuilder {
   public build(): Tray {
     try {
       const tray = new Tray(nativeImage.createFromPath(this.iconPath));
-      if (this.toolTip) tray.setToolTip(this.toolTip);
+      if (this.toolTip) {
+        tray.setToolTip(this.toolTip);
+      }
 
-      if (this.contextMenu) tray.setContextMenu(this.contextMenu.build());
+      if (this.contextMenu) {
+        JsonUtils.modifyObject(
+          this.contextMenu,
+          ["label"],
+          (_, value: unknown) => {
+            return TranslatorMain.translate(value as string);
+          }
+        );
+        tray.setContextMenu(Menu.buildFromTemplate(this.contextMenu));
+      }
 
       return tray;
     } catch (error) {
       LoggerMain.error("Error creating tray icon", error);
       throw error;
     }
-  }
-}
-
-export class TrayMenuBuilder {
-  private entries: Array<MenuItemConstructorOptions> = [];
-
-  private constructor() {}
-
-  public static builder(): TrayMenuBuilder {
-    return new TrayMenuBuilder();
-  }
-
-  public addLabel(
-    label: string,
-    click: (() => void) | undefined = undefined
-  ): TrayMenuBuilder {
-    this.entries.push({
-      label,
-      type: "normal",
-      click,
-    });
-    return this;
-  }
-
-  public addSeparator(): TrayMenuBuilder {
-    this.entries.push({ type: "separator" });
-    return this;
-  }
-
-  public build(): Menu {
-    for (let i = 0; i < this.entries.length; i++) {
-      if (this.entries[i]["label"]) {
-        this.entries[i]["label"] = TranslatorMain.translate(
-          this.entries[i]["label"] as string
-        );
-      }
-    }
-    return Menu.buildFromTemplate(this.entries);
   }
 }
