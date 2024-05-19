ed23f6180ad3a4c461bdc1c184f1cc17fdfd5141
Emilio <ojosdeserbio@gmail.com>
2024-05-19 16:48:34 +0200

    no message

--- src/implementations/Powershell.ts
+++ src/implementations/Powershell.ts
@@ -2,16 +2,12 @@ import { spawn } from "child_process";
 
 export class Powershell {
   private static PROCESS = spawn("powershell");
-  private static OUTPUT = "";
   private static COMMANDS: Array<string> = [];
   private static RESOLVERS: Array<(data: string) => void> = [];
   private static IS_RUNNING: boolean = false;
   private static ACTUAL_OUTPUT: boolean = false;
 
   public static initialize() {
-    Powershell.PROCESS.stderr.on("data", function (data: string) {
-      Powershell.OUTPUT += data;
-    });
     Powershell.PROCESS.stdout.on("data", function (data: string) {
       const lines = data.toString().split("\n");
       if (lines[lines.length - 1].trim() == "") {
@@ -19,16 +15,12 @@ export class Powershell {
       }
       if (lines[lines.length - 1].startsWith("PS ")) {
         if (Powershell.ACTUAL_OUTPUT && Powershell.RESOLVERS.length > 0) {
-          Powershell.RESOLVERS[0](Powershell.OUTPUT);
+          Powershell.RESOLVERS[0](data);
           Powershell.RESOLVERS.shift();
           Powershell.IS_RUNNING = false;
-          Powershell.OUTPUT = "";
         } else {
           Powershell.ACTUAL_OUTPUT = true;
-          Powershell.OUTPUT = "";
         }
-      } else {
-        Powershell.OUTPUT += data;
       }
     });
 
