9fd47ba4e38fd56bc7954892b034368f5510de83
Emilio <ojosdeserbio@gmail.com>
2024-05-19 16:42:40 +0200

    no message

--- src/implementations/Powershell.ts
+++ src/implementations/Powershell.ts
@@ -6,6 +6,7 @@ export class Powershell {
   private static COMMANDS: Array<string> = [];
   private static RESOLVERS: Array<(data: string) => void> = [];
   private static IS_RUNNING: boolean = false;
+  private static ACTUAL_OUTPUT: boolean = false;
 
   public static initialize() {
     Powershell.PROCESS.stderr.on("data", function (data: string) {
@@ -17,10 +18,13 @@ export class Powershell {
         lines.pop();
       }
       if (lines[lines.length - 1].startsWith("PS ")) {
-        if (Powershell.RESOLVERS.length > 0) {
+        if (Powershell.ACTUAL_OUTPUT && Powershell.RESOLVERS.length > 0) {
           Powershell.RESOLVERS[0](Powershell.OUTPUT);
           Powershell.RESOLVERS.shift();
           Powershell.IS_RUNNING = false;
+          Powershell.OUTPUT = "";
+        } else {
+          Powershell.ACTUAL_OUTPUT = true;
           Powershell.OUTPUT = "";
         }
       } else {
