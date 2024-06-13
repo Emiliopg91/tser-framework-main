f5fd2d219aa297bafc47fc8056860dadef47d31a
Emilio <ojosdeserbio@gmail.com>
2024-06-13 12:01:01 +0200

    no message

--- src/implementations/AppUpdater.ts
+++ src/implementations/AppUpdater.ts
@@ -1,15 +1,56 @@
-import { autoUpdater } from 'electron-updater';
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Event, app } from 'electron';
+import { UpdateDownloadedEvent, autoUpdater } from 'electron-updater';
 import { AppUpdaterEvents } from 'electron-updater/out/AppUpdater';
 
+import { DateUtils } from './DateUtils';
 import { LoggerMain } from './LoggerMain';
 
 export class AppUpdater {
-  public constructor() {
-    autoUpdater.logger = new LoggerMain('AppUpdater');
+  private static LOGGER = new LoggerMain('AppUpdater');
+  private static CHECK_TIMEOUT = 60 * 1000;
+
+  public constructor(callback?: (event: UpdateDownloadedEvent) => void) {
+    autoUpdater.logger = {
+      info(..._args: any): void {},
+      warn(..._args: any): void {},
+      error(..._args: any): void {}
+    };
     autoUpdater.autoDownload = true;
     autoUpdater.disableWebInstaller = true;
     autoUpdater.forceDevUpdateConfig = true;
     autoUpdater.disableDifferentialDownload = true;
+
+    autoUpdater.checkForUpdates();
+
+    autoUpdater.on('checking-for-update', (): void => {
+      AppUpdater.LOGGER.info('Checking for ' + app.getName() + ' update');
+    });
+
+    autoUpdater.on('update-not-available', (info): void => {
+      setTimeout(async () => {
+        await autoUpdater.checkForUpdates();
+      }, AppUpdater.CHECK_TIMEOUT);
+      AppUpdater.LOGGER.system(
+        'Next update check: ' +
+          DateUtils.dateToFormattedString(new Date(new Date().getTime() + 24 * 60 * 60 * 1000))
+      );
+    });
+
+    autoUpdater.on('update-available', (info): void => {
+      AppUpdater.LOGGER.system('Available ' + info.version + ' update, starting download');
+    });
+
+    autoUpdater.on('update-downloaded', (info: UpdateDownloadedEvent): void => {
+      AppUpdater.LOGGER.system('Finished download');
+      if (callback) {
+        callback(info);
+      }
+    });
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
